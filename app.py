8d3cdbd595d48011215364c5519c92e3f3b6e9d9
alihassanml <alihassancsml@gmail.com>
2025-09-05 01:46:54 +0500

    first commit

--- app.py
+++
@@ -1,7 +0,0 @@
-import smtplib
-
-server = smtplib.SMTP("smtp.gmail.com", 587)
-server.starttls()
-server.login("contact@apexaihome.com", "dvbauvfhntwbqrpo")
-server.sendmail("contact@apexaihome.com", "alihassanbscs99@gmail.com", "Test email from Python")
-server.quit()
